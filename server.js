bc1c3607168e6a6f590b3a137b10b7e9d1890d61
Amanda Faulkenberry <amandalfwrites@gmail.com>
2022-07-30 22:49:04 -0400

    adding changes for config

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
+const bodyparser = require('body-parser')
 const app = express()
 
+dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
 
-dotenv.config({path:'config.env'})
+//parse request to body-parser
+app.usee(bodyparser.urlencoded({extended: true}))
+
+//set view engine
+app.set('view engine', 'ejs')
+app.set('views',)
 
 app.get('/', (request, response)=>{
     response.send('Classroom Management Application')
